3aae05324092b7f002ca12f5dad81a82b72da6d0
Takehito Oshita <oshita.takehito@brains-consulting.co.jp>
2019-03-25 20:53:35 +0900

    modified: changed help message

--- examples/train_mnists.py
+++ examples/train_mnists.py
@@ -13,22 +13,22 @@ from npmodel.datasets.mnists import NPMnistReader, NPBatches, show_yimages
 
 def get_args():
     parser = argparse.ArgumentParser(description='VAE MNIST Example')
-    parser.add_argument('--lr', type=float, default=1e-4, metavar='F',
+    parser.add_argument('--lr', type=float, default=1e-4, metavar='RATE',
                         help='learning rate (default: 1e-4)')
     parser.add_argument('--batch-size', type=int, default=20, metavar='N',
-                        help='input batch size for training (default: 8)')
+                        help='input batch size for training (default: 20)')
     parser.add_argument('--epochs', type=int, default=100, metavar='N',
-                        help='number of epochs to train (default: 10)')
+                        help='number of epochs to train (default: 100)')
     parser.add_argument('--no-cuda', action='store_true', default=False,
                         help='enables CUDA training')
     parser.add_argument('--gpu', type=int, default=0, metavar='N',
                         help='gpu number (default: 0), if no-cuda, ignore this option.')
-    parser.add_argument('--seed', type=int, default=777, metavar='S',
+    parser.add_argument('--seed', type=int, default=777, metavar='N',
                         help='random seed (default: 777)')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
-    parser.add_argument('--dataset', type=str, default="mnist", choices=["mnist", "fashion"], metavar='S',
-                        help='dataset name like "mnist", "fashion-mnist", default: "mnist"')
+                        help='how many batches to wait before logging training status (default: 10)')
+    parser.add_argument('--dataset', type=str, default="mnist", choices=["mnist", "fashion", "kmnist", "emnist"],
+                        help='dataset name like "mnist", "fashion-mnist" (default: "mnist")')
     parser.add_argument('--fix-iter', type=int, default=1000, metavar='N',
                         help='the number of training iteration with a fixed sampling dataset/imageset batch'
                              ', if negative use whole dataset (default: 1000)')
