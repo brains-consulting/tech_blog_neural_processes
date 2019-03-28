254262501ef80fd319496d48554a6478bc78f3f7
Takehito Oshita <oshita.takehito@brains-consulting.co.jp>
2019-03-28 14:42:08 +0900

    modified: remove "emnist" from args.dataset

--- examples/train_mnists.py
+++ examples/train_mnists.py
@@ -27,7 +27,7 @@ def get_args():
                         help='random seed (default: 777)')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                         help='how many batches to wait before logging training status (default: 10)')
-    parser.add_argument('--dataset', type=str, default="mnist", choices=["mnist", "fashion", "kmnist", "emnist"],
+    parser.add_argument('--dataset', type=str, default="mnist", choices=["mnist", "fashion", "kmnist"],
                         help='dataset name for the several mnist datasets (default: "mnist")')
     parser.add_argument('--fix-iter', type=int, default=1000, metavar='N',
                         help='the number of training iteration with a fixed sampling dataset/imageset batch'
